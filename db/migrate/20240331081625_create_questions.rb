7a182ac99e630bc183bfbc48e1981d6fa49d264a
mnmnkm <152707508+mnmnkm@users.noreply.github.com>
2024-05-18 09:10:45 +0000

    migration修正

--- db/migrate/20240331081625_create_questions.rb
+++ db/migrate/20240331081625_create_questions.rb
@@ -5,7 +5,6 @@ class CreateQuestions < ActiveRecord::Migration[6.1]
       t.string :title
       t.text :body
       t.boolean :is_active, null: false, default: true
-  　　#解決/未解決、公開/非公開のbooleanを設定したい。2つ書くべきか。
       t.timestamps
     end
   end
