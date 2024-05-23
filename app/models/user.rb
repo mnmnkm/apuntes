3c7b0eef817b3acb54660bb187c508eaab257e1c
mnmnkm <152707508+mnmnkm@users.noreply.github.com>
2024-05-23 15:19:21 +0000

    [Fix]画像サイズ固定

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,12 +19,14 @@ class User < ApplicationRecord
          
   has_one_attached :profile_image
   
-  def get_profile_image(x,y)
+  def get_profile_image
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    profile_image.variant(resize_to_limit: [x, y]).processed
+    #profile_image.variant(resize_to_limit: [x, y]).processed
+    # profile_image.variant(gravity: :center, resize:"#{x}x#{y}^", crop:"#{x}x#{y}+0+0").processed
+    profile_image.variant(gravity: :center, resize:"640x640^", crop:"640x640+0+0").processed
   end
   
   # def self.search_for(keyword)
